2761aeaf3f73d3f18b45a5e90427bd9e7cace9f3
liam2534 <liam25@uw.edu>
2024-02-21 10:12:43 -0800

    Almost done

--- A6-Folder/a6ArduinoSketch/a6ArduinoSketch.ino
+++ A6-Folder/a6ArduinoSketch/a6ArduinoSketch.ino
@@ -1,21 +1,32 @@
+//runs setup once
 void setup() {
+  //sets pin 5 to output for LED
   pinMode(5, OUTPUT);
+  //Starts the serial monitor
   Serial.begin(9600);
-
-
 }
 
+//loop runs repeatedly
 void loop() {
-  // put your main code here, to run repeatedly:
+  //if there is available serial from p5
   if (Serial.available()){
+    //if it reads 0, turns light off. Otherwise, turns it off.
     if(Serial.read() == 0){
+      //turns light on
       digitalWrite(5, HIGH);
+      //enters this branch when Serial.read != 0
     } else{
+      //if Serial.read() != 0, turns LED off
       digitalWrite(5, LOW);
     }
   }
-
-  Serial.print(analogRead(A0)); //X Value
+  
+  //prints X value (from joystick) from A0 to serial monitor
+  Serial.print(analogRead(A0));
+  //prints a comma to separate X and Y value (see below) 
   Serial.print(",");
-  Serial.println(analogRead(A1)); //Y Value
+  //prints Y value (from joystick) from A1 to the same line after the comma and then makes a new line
+  Serial.println(analogRead(A1));
+  //add 50ms delay
+  delay(50);
 }
--- A6-Folder/a6Code/index.js
+++ A6-Folder/a6Code/index.js
@@ -1,11 +1,16 @@
-const BAUD_RATE = 9600; // This should match the baud rate in your Arduino sketch
+//baud rate matches arduino
+const BAUD_RATE = 9600;
 
-let port, connectBtn; // Declare global variables
+//global variables to be used later for the port and connect button
+let port, connectBtn;
 
+//variable for txt color to be determined later
 let txtColor = "idk_yet";
 
+//runs once
 function setup() {
-  setupSerial(); // Run our serial setup function (below)
+  // Run our serial setup function (below)
+  setupSerial(); 
 
   // Create a canvas that is the size of our browser window.
   // windowWidth and windowHeight are p5 variables
@@ -14,39 +19,63 @@ function setup() {
   // p5 text settings. BOLD and CENTER are constants provided by p5.
   // See the "Typography" section in the p5 reference: https://p5js.org/reference/
   textFont("system-ui", 50);
+  //makes text bold
   textStyle(BOLD);
+  //makes text in the center of the screen
   textAlign(CENTER, CENTER);
   
 }
 
+//draw function runs repeatedly
 function draw() {
+  //makes a const portIsOpen value equal to checkPort
   const portIsOpen = checkPort();
+  //checks if port is open, if not, returns.
   if (!portIsOpen) return;
 
+  //makes a string of the whole line from the serial monitor
   let str = port.readUntil("\n");
+  //if string length is empty, returns.
   if (str.length == 0) return; 
 
+  //makes an array from the comma separated values from the serial monitor from the arduino
   let valArray = str.trim().split(",");
   
-  if(valArray[0] <= 600){
+  //checks if the first value in the array is <= 600
+  if(Number(valArray[0]) <= 600){
+    //if yes, makes background color red
     background("red");
+    //else statement if valArray[0] fails first check
   } else{
+    //makes background color black
     background("black");
   }
 
-  if(valArray[1] <= 600){
+  //check for value of valArray[1]
+  if(Number(valArray[1]) <= 600){
+    //if the number is <= 600, text color is white
     txtColor = "white";
+    //else if first if fails
   } else {
+    //makes text color gold if first check fails
     txtColor = "gold";
   }
 
+  //check for if mouse is pressed
   if(mouseIsPressed){
+    //makes the text in center (see text line in this if branch) correspond to txtColor
     fill(txtColor);
+    //if mouseIsPressed, displays the bottom message in the center of screen
     text("YOU ARE NOW PRESSING THE MOUSE, AND THE LED IS ON!", windowWidth / 2, windowHeight / 2);
+    //writes 0 to the port, which is used by the arduino to turn light on
     port.write(0);
+    //else branch for when mouse is not pressed
   } else{
+    //makes text below correspond to txtColor
     fill(txtColor);
+    //displays this text in the center of the screen
     text("PRESS LEFT MOUSE BUTTON TO TOGGLE LED", windowWidth / 2, windowHeight / 2);
+    //writes 1 to port, used by arduino to toggle the light off.
     port.write(1);
   }
 
@@ -54,7 +83,9 @@ function draw() {
 
 // Three helper functions for managing the serial connection.
 
+//setup port to read in the serial monitor
 function setupSerial() {
+  //makes port read from serial monitor
   port = createSerial();
 
   // Check to see if there are any ports we have used previously
