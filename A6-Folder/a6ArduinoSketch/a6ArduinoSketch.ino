949e85b025c93b9edda4880f927a987f58c7960c
liam2534 <liam25@uw.edu>
2024-02-20 16:46:49 -0800

    many changes

---
+++ A6-Folder/a6ArduinoSketch/a6ArduinoSketch.ino
@@ -0,0 +1,22 @@
+void setup() {
+  pinMode(5, OUTPUT);
+  Serial.begin(9600);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available()){
+    if(Serial.read() == 0){
+      digitalWrite(5, HIGH);
+    } else{
+      digitalWrite(5, LOW);
+    }
+  }
+
+  Serial.println(analogRead(A0)); //X Value
+  Serial.println(analogRead(A1)); //Y Value
+
+  delay(500);
+}
