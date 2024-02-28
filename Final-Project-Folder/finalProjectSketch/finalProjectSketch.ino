876dd9fffcc4a1386547f4a5a6f75a1e5530a80b
liam2534 <liam25@uw.edu>
2024-02-27 16:58:39 -0800

    Good start

--- Final-Project-Folder/finalProjectSketch/finalProjectSketch.ino
+++ Final-Project-Folder/finalProjectSketch/finalProjectSketch.ino
@@ -5,36 +5,43 @@
   //int for servo rotation value
   #include <CapacitiveSensor.h>
 
+  //current value int to be used in loop section
   int currentVal = 0;
+  //last value int to be compared to currentVal int later in loop section
   int lastVal = 0;
   
+  //Making a capacitive sensor object C
+  CapacitiveSensor   C = CapacitiveSensor(4,2);
 
-  CapacitiveSensor   C = CapacitiveSensor(4,2);    	// 1 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, metal thing
-
-  
   //setup code runs once upon startup
   void setup() {
-    C.set_CS_AutocaL_Millis(0xFFFFFFFF); 	// turn off autocalibrate on channel 1 - just as an example
+    //starts the serial monitor
     Serial.begin(9600);
 
     //adding servo motor to pin 11
     S.attach(9);
+    //sets the servo motor's position to 0 degrees
     S.write(0);
   }
   
   //loop runs repeatedly
   void loop() {
+    //reads in the current value from the capactitiveSensor() function, takes in 30, which is the number of times the sensor is measured, the elapsed time is
+    //returned in "arbitrary units" (according to the documentation) for this library. Generally speaking, the higher the number returned, the more contact is being made with the sensor
     currentVal = C.capacitiveSensor(30);
 
-    
+    //checks if currentVal is > 2000 and if lastVal is < 2000
+    //if it is the case, the servo motor moves to dispense the treat
     if (currentVal > 2000 && lastVal < 2000){
-      S.write(50);
+      //moves the servo to 60 degrees
+      S.write(60);
+      //delays 100ms to give enough time for servo to move
       delay(100);
+      //resets servo position to 0
       S.write(0);
+      //delays one second
       delay(1000);
     }
-
-    Serial.println(currentVal);
-
+    //sets lastVal = currentVal to ensure the if statement works properly
     lastVal = currentVal;
   }            
